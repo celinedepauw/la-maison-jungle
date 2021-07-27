4ef8c0d839071e9143a9c6aea10641a90a78ff5e
celin <celin@localhost>
2021-07-27 16:04:29 +0200

    add a component QuestionForm

---
+++ src/components/QuestionForm/index.js
@@ -0,0 +1,17 @@
+const QuestionForm = () => {
+
+    const [inputValue, setInputValue] = useState("Posez votre question ici");
+    return(
+        <div className="questionForm">
+            <textarea
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+            />
+            <button onClick={() => alert(inputValue)}>
+                Alertez-moi !!
+            </button>
+        </div>
+    )
+}
+
+export default QuestionForm;
